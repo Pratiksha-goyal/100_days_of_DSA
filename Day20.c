25adb347f1dbc36ba8fdd7175812cf0299cd7430
Pratiksha Goyal <pratikshagoyal01@gmail.com>
2026-02-20 23:45:35 +0530

    Day20_of_DSA

---
+++ Day20.c
@@ -0,0 +1,45 @@
+// Given an array of integers, count the number of subarrays whose sum is equal to zero
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int n;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter %d integers:\n", n);
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int prefixSum = 0;
+    int count = 0;
+
+    int size = 200001;
+    int offset = 100000;
+
+    int *freq = (int*)calloc(size, sizeof(int));
+
+    freq[offset] = 1;
+
+    for(int i = 0; i < n; i++) {
+        prefixSum += arr[i];
+
+        int index = prefixSum + offset;
+
+        if(index >= 0 && index < size) {
+            count += freq[index];
+            freq[index]++;
+        }
+    }
+
+    printf("\nNumber of subarrays with sum = 0 is: %d\n", count);
+
+    free(freq);
+
+    return 0;
+}
