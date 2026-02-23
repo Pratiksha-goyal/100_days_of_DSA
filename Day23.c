b841f56c15f1a44a23ce4db0215ba3387a004381
Pratiksha Goyal <pratikshagoyal01@gmail.com>
2026-02-23 23:04:55 +0530

    Day23_of_DSA

---
+++ Day23.c
@@ -0,0 +1,101 @@
+// Merge Two Sorted Linked Lists - Implement using linked list with dynamic memory allocation.
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* createNode(int value) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    if (newNode == NULL) {
+        printf("Memory allocation failed!\n");
+        exit(1);
+    }
+    newNode->data = value;
+    newNode->next = NULL;
+    return newNode;
+}
+
+struct Node* insertEnd(struct Node* head, int value) {
+    struct Node* newNode = createNode(value);
+
+    if (head == NULL)
+        return newNode;
+
+    struct Node* temp = head;
+    while (temp->next != NULL)
+        temp = temp->next;
+
+    temp->next = newNode;
+    return head;
+}
+
+struct Node* mergeLists(struct Node* l1, struct Node* l2) {
+    struct Node dummy;  
+    struct Node* tail = &dummy;
+    dummy.next = NULL;
+
+    while (l1 != NULL && l2 != NULL) {
+        if (l1->data <= l2->data) {
+            tail->next = l1;
+            l1 = l1->next;
+        } else {
+            tail->next = l2;
+            l2 = l2->next;
+        }
+        tail = tail->next;
+    }
+
+    if (l1 != NULL)
+        tail->next = l1;
+    else
+        tail->next = l2;
+
+    return dummy.next;
+}
+
+void printList(struct Node* head) {
+    if (head == NULL) {
+        printf("List is empty.\n");
+        return;
+    }
+
+    while (head != NULL) {
+        printf("%d ", head->data);
+        head = head->next;
+    }
+    printf("\n");
+}
+
+int main() {
+    struct Node* list1 = NULL;
+    struct Node* list2 = NULL;
+    int n, m, value;
+
+    printf("Enter number of elements in first sorted list: ");
+    scanf("%d", &n);
+
+    printf("Enter %d sorted elements:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &value);
+        list1 = insertEnd(list1, value);
+    }
+
+    printf("Enter number of elements in second sorted list: ");
+    scanf("%d", &m);
+
+    printf("Enter %d sorted elements:\n", m);
+    for (int i = 0; i < m; i++) {
+        scanf("%d", &value);
+        list2 = insertEnd(list2, value);
+    }
+
+    struct Node* mergedList = mergeLists(list1, list2);
+
+    printf("Merged Sorted List:\n");
+    printList(mergedList);
+
+    return 0;
+}
