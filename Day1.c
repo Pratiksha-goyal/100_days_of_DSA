e106fa082dcde8447f4ab60f7df117f166ae8549
Pratiksha Goyal <pratikshagoyal01@gmail.com>
2026-03-05 23:45:28 +0530

    Day1_of_DSA
    Refactor input handling and improve comments.

--- Day1.c
+++ Day1.c
@@ -2,31 +2,25 @@
 
 int main() {
     int n, pos, x;
-    int arr[100];   // enough size for insertion
-
-    // Input size
+    int arr[100];  
+   
     scanf("%d", &n);
 
-    // Input array
+
     for (int i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
 
-    // Input position (1-based)
     scanf("%d", &pos);
 
-    // Input element to insert
     scanf("%d", &x);
 
-    // Shift elements to right
     for (int i = n; i >= pos; i--) {
         arr[i] = arr[i - 1];
     }
 
-    // Insert element
     arr[pos - 1] = x;
 
-    // Print updated array
     for (int i = 0; i <= n; i++) {
         printf("%d ", arr[i]);
     }
