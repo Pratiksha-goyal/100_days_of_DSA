eb7400d8da4fccc4ebf5e65cb9baaeb689088baa
Pratiksha Goyal <pratikshagoyal01@gmail.com>
2026-03-21 21:28:39 +0530

    Day49_of_DSA

---
+++ Day49.c
@@ -0,0 +1,58 @@
+//  BST Insert
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+struct Node* createNode(int val) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = val;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+
+struct Node* insert(struct Node* root, int key) {
+    if (root == NULL) {
+        return createNode(key);
+    }
+
+    if (key < root->data) {
+        root->left = insert(root->left, key);
+    }
+    else if (key > root->data) {
+        root->right = insert(root->right, key);
+    }
+
+    return root;
+}
+
+void inorder(struct Node* root) {
+    if (root == NULL) return;
+
+    inorder(root->left);
+    printf("%d ", root->data);
+    inorder(root->right);
+}
+
+int main() {
+    struct Node* root = NULL;
+    int n, val;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter elements:\n");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &val);
+        root = insert(root, val);
+    }
+
+    printf("Inorder traversal: ");
+    inorder(root);
+
+    return 0;
+}
