caabfa9bc7b14a2219b90ec8b8f1c1dc58ff0f6e
Pratiksha Goyal <pratikshagoyal01@gmail.com>
2026-04-03 17:19:42 +0530

    Day62_of_DSA

---
+++ Day62.c
@@ -0,0 +1,59 @@
+// Build a graph with adjacency list representation. Use linked lists or dynamic arrays.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int vertex;
+    struct Node* next;
+};
+
+struct Node* createNode(int v) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->vertex = v;
+    newNode->next = NULL;
+    return newNode;
+}
+
+int main() {
+    int n, m, i, u, v;
+
+    printf("Enter number of vertices: ");
+    scanf("%d", &n);
+
+    printf("Enter number of edges: ");
+    scanf("%d", &m);
+
+    struct Node* adj[n];
+
+    for (i = 0; i < n; i++) {
+        adj[i] = NULL;
+    }
+
+    printf("Enter edges (u v):\n");
+    for (i = 0; i < m; i++) {
+        scanf("%d %d", &u, &v);
+
+        struct Node* newNode = createNode(v);
+        newNode->next = adj[u];
+        adj[u] = newNode;
+
+        newNode = createNode(u);
+        newNode->next = adj[v];
+        adj[v] = newNode;
+    }
+
+    printf("\nAdjacency List:\n");
+    for (i = 0; i < n; i++) {
+        printf("%d -> ", i);
+
+        struct Node* temp = adj[i];
+        while (temp != NULL) {
+            printf("%d -> ", temp->vertex);
+            temp = temp->next;
+        }
+        printf("NULL\n");
+    }
+
+    return 0;
+}
