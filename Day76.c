9ec46eb35ef4b616215c3c17621b91a4b0add1c7
Pratiksha Goyal <pratikshagoyal01@gmail.com>
2026-04-22 17:10:16 +0530

    Day76_of_DSA

--- Day76.c
+++ Day76.c
@@ -3,11 +3,10 @@
 
 #define MAX 100
 
-int adj[MAX][MAX];   // Adjacency matrix
-int visited[MAX];    // Visited array
+int adj[MAX][MAX];   
+int visited[MAX];    
 int n, m;
 
-// DFS function
 void dfs(int node) {
     visited[node] = 1;
 
@@ -22,10 +21,8 @@ int main() {
     int u, v;
     int components = 0;
 
-    // Input
     scanf("%d %d", &n, &m);
 
-    // Initialize adjacency matrix and visited array
     for (int i = 1; i <= n; i++) {
         visited[i] = 0;
         for (int j = 1; j <= n; j++) {
@@ -33,14 +30,12 @@ int main() {
         }
     }
 
-    // Read edges
     for (int i = 0; i < m; i++) {
         scanf("%d %d", &u, &v);
         adj[u][v] = 1;
-        adj[v][u] = 1;  // Undirected graph
+        adj[v][u] = 1;  
     }
 
-    // Count connected components
     for (int i = 1; i <= n; i++) {
         if (visited[i] == 0) {
             dfs(i);
@@ -48,7 +43,6 @@ int main() {
         }
     }
 
-    // Output
     printf("%d\n", components);
 
     return 0;
